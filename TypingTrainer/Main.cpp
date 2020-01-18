9814c334407dea179e38f809abc3313c6fd05b69
821055716195 <qazw181900@gmail.com>
2020-01-18 15:23:42 +0900

    save

--- TypingTrainer/Main.cpp
+++ TypingTrainer/Main.cpp
@@ -1,11 +1,13 @@
 // TypingTrainer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-#pragma once
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
 #include <vector>
-#include "TypingTrainer.h"
+#include <algorithm>
+#include "Screen.h"
+#include "Utill.h"
+
 
 using namespace std;
 // https://media.discordapp.net/attachments/293378188979994624/667216288782876702/unknown.png?width=481&height=670
@@ -17,68 +19,91 @@ using namespace std;
 //        cout << buf[i];
 //    }
 //}
-// 75 left 77 right
+
 int main()
 {
 
-	char c;
-
-	c = _getch();
-
-	if (c == 13) {
-
-		system("cls");
-
-		TypingTrainer trainer;
-
-		trainer.UtillManageMent();
-	}
-
 	//FileManager manager;
-//     Screen sc(60,40);
-//     Widget t1("Typing Trainer", 20, 20);
-//     sc.AddWidget(t1);
-//     sc.Show();
-//     char a;
-//     cin >> a;
 
-	//char c;
-	//vector<char> buf;
-	//while (true) 
-	//{ 
-	//    c = _getch();
-	//    if (c == 27) {
-	//        break;
-	//    }
+	//manager.RoadRanking();
+	//manager.ShowRanking();
+	//manager.Realignment('A', "UP");
+	//manager.ShowRanking();
+	//manager.SaveRanking();
 
-	//    if (c == '\b') {
-	//        if (buf.empty() == false) {
-	//            buf.pop_back();
-	//        }
-	//        //PrintBuf(buf);
-	//        cout << "\b \b";
-	//    }
-	//    else if (c == 13) {
-	//        cout << endl;
-	//    }
-	//    else {
-	//        buf.push_back(c);
-	//        cout << c;
-	//    }
-	//
-	//}
+	////FileManager manager;
+	Screen sc(60, 40);
+	Screen sc2(60, 40);
+	Screen sc3(60, 40);
+	Widget* t1 = new Widget("Typing Trainer", 20, 20);
+	Widget* t2 = new Button(&sc2, "screen2", 20, 30);
+	Widget* t3 = new Button(&sc3, "screen3", 20, 32);
+	sc.AddWidget(t1);
+	sc.AddWidget(t2);
+	sc.AddWidget(t3);
+	sc.SetFocus(1);
+	//sc.Show();
+	//char a;
+	//cin >> a;
+
+	char c;
+	vector<char> buf;
+
+	Screen current;
+	current = sc;
+	const vector<int>* selectableIndices = current.GetSelectableIndices();
+	while (true)
+	{
+		current.Show();
+		c = _getch();
+		if (c == 27) {
+			break;
+		}
+		//if (c == '\b') {
+		//    if (buf.empty() == false) {
+		//        buf.pop_back();
+		//    }
+		//    //PrintBuf(buf);
+		//    cout << "\b \b";
+		//}
+		//else if (c == 13) {
+		//    cout << endl;
+		//}
+		//else {
+		//    buf.push_back(c);
+		//    cout << c;
+		//}
+		if (c == 72)
+		{
+			// upwards arrow
+			int size = selectableIndices->size();
+			int focus = current.GetFocus();
+			if (focus > 0)
+			{
+				current.SetFocus(focus - 1);
+			}
+
+		}
+		else if (c == 80)
+		{
+			// downwards arrow
+			int size = selectableIndices->size();
+			int focus = current.GetFocus();
+			if (focus < size - 1)
+			{
+				current.SetFocus(focus + 1);
+			}
+		}
+		else if (c == 32)
+		{
+			int focus = current.GetFocus();
+			current = *current.GetWidgets()[selectableIndices->at(focus)]->GetNext();
+			selectableIndices = current.GetSelectableIndices();
+		}
+
+
+	}
 	return 0;
 }
 
 
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
--- TypingTrainer/TypingTrainer.cpp
+++ TypingTrainer/TypingTrainer.cpp
@@ -1,109 +1,65 @@
-﻿// TypingTrainer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-#include <iostream>
-#include <conio.h>
-#include <cstdlib>
-#include <vector>
-#include <algorithm>
-#include "Screen.h"
-#include "Utill.h"
+﻿#include "TypingTrainer.h"
 
+TypingTrainer::TypingTrainer() {
 
-using namespace std;
-// https://media.discordapp.net/attachments/293378188979994624/667216288782876702/unknown.png?width=481&height=670
-//void PrintBuf(const vector<char>& buf)
-//{
-//    system("cls");
-//    for (int i = 0; i < buf.size(); i++)
-//    {
-//        cout << buf[i];
-//    }
-//}
-
-int main() 
-{
-
-	//FileManager manager;
-
-	//manager.RoadRanking();
-	//manager.ShowRanking();
-	//manager.Realignment('A', "UP");
-	//manager.ShowRanking();
-	//manager.SaveRanking();
-
-	////FileManager manager;
-     Screen sc(60,40);
-     Screen sc2(60, 40);
-     Screen sc3(60, 40);
-     Widget* t1 = new Widget("Typing Trainer", 20, 20);
-     Widget* t2 = new Button(&sc2, "screen2", 20, 30);
-     Widget* t3 = new Button(&sc3, "screen3", 20, 32);
-     sc.AddWidget(t1);
-     sc.AddWidget(t2);
-     sc.AddWidget(t3);
-     sc.SetFocus(1);
-     //sc.Show();
-     //char a;
-     //cin >> a;
-   
-    char c;
-    vector<char> buf;
-    
-    Screen current;
-    current = sc;
-    const vector<int>* selectableIndices = current.GetSelectableIndices();
-    while (true) 
-    { 
-        current.Show();
-        c = _getch();
-        if (c == 27) {
-            break;
-        }
-        //if (c == '\b') {
-        //    if (buf.empty() == false) {
-        //        buf.pop_back();
-        //    }
-        //    //PrintBuf(buf);
-        //    cout << "\b \b";
-        //}
-        //else if (c == 13) {
-        //    cout << endl;
-        //}
-        //else {
-        //    buf.push_back(c);
-        //    cout << c;
-        //}
-        if (c == 72)
-        {
-            // upwards arrow
-            int size = selectableIndices->size();
-            int focus = current.GetFocus();
-            if (focus > 0)
-            {
-                current.SetFocus(focus - 1);
-            }
-
-        } 
-        else if (c == 80)
-        {
-            // downwards arrow
-            int size = selectableIndices->size();
-            int focus = current.GetFocus();
-            if (focus < size - 1)
-            {
-                current.SetFocus(focus + 1);
-            }
-        }
-        else if (c == 32)
-        {
-            int focus = current.GetFocus();
-            current = *current.GetWidgets()[selectableIndices->at(focus)]->GetNext();
-            selectableIndices = current.GetSelectableIndices();
-        }
-        
-    
-    }
-    return 0;
 }
 
+TypingTrainer::TypingTrainer(const Screen& screen) {
 
+	Screen tmp;
+	vector<Widget> widgets;
+
+	Cur = screen;
+	//widgets = Cur.GetWidgets();
+
+	for (int i = 0; i < widgets.size(); i++) {
+		//tmp = widgets[i]; // 위젯 클래스에서 가리키는 특정 스크린을 뽑아서
+		//mSubScreen.push_back(tmp); // 현재 스크린의 서브스크린들에 추가
+
+	}
+}
+
+TypingTrainer::~TypingTrainer() {
+
+}
+
+void TypingTrainer::ScreenSwitching(const Screen& screen) { // 특정 스크린 자체를 변수로 받거나, 번호를 받아서 서브스크린에서 뽑아 사용
+	screen.Show();
+}
+
+void TypingTrainer::UtillManageMent() {
+
+	char type, order;
+
+	FileManager manager;
+
+	manager.RoadRanking();
+	manager.ShowRanking();
+
+	while (true)
+	{
+
+		type = _getch();
+		if (type == 27) {
+			break;
+		}
+
+		cout << type;
+
+		order = _getch();
+		order = _getch();
+
+		if (type == 'a' || type == 'c' || type == 't' || type == 'n') {
+			type = type - 32;
+			if ((int)order == 72) manager.Realignment(type, "UP");
+			else if ((int)order == 80) manager.Realignment(type, "DOWN");
+		}
+
+		system("cls");
+
+		manager.ShowRanking();
+
+	}
+
+
+}
