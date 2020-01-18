513766e344ba57e2ed3905ad45e38d04fe45b0d1
verssae <tiger2714@naver.com>
2020-01-18 15:07:58 +0900

    0307

--- TypingTrainer/Screen.cpp
+++ TypingTrainer/Screen.cpp
@@ -14,12 +14,13 @@ Screen::Screen()
 	: mWidth(60),
 	mHeight(30),
 	mFocus(0),
-	mWidgets()
+	mWidgets(),
+	mSelectableIndices()
 {
 }
 
 Screen::Screen(int x, int y)
-	: mWidth(x), mHeight(y), mFocus(0), mWidgets()
+	: mWidth(x), mHeight(y), mFocus(0), mWidgets(), mSelectableIndices()
 {
 }
 
@@ -27,12 +28,22 @@ Screen::~Screen()
 {
 }
 
+void Screen::SetSelectableIndices()
+{
+	for (int i = 0; i < mWidgets.size(); i++)
+	{
+		if (mWidgets[i]->GetSelectable())
+		{
+			mSelectableIndices.push_back(i);
+		}
+	}
+}
+
 void Screen::Show() const {
 
 	COORD cur;
 	console::SetCursorVisible(false);
     console::SetWindowSize(mWidth, mHeight);
-	console::SetColor(FOREGROUND_BLUE | BACKGROUND_GREEN);
 
     cur.X = 0;
     cur.Y = 0;
@@ -40,62 +51,37 @@ void Screen::Show() const {
     cout << setw(mWidth) << setfill('=') << "" ;
 	
 
-	for (size_t i = 0; i < mWidgets.size(); i++) {
-		cur.X = mWidgets[i].GetX();
-		cur.Y = mWidgets[i].GetY();
+	for (size_t i = 0; i < mWidgets.size(); i++) 
+	{
+		cur.X = mWidgets[i]->GetX();
+		cur.Y = mWidgets[i]->GetY();
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
-		cout << mWidgets[i].GetString();
+
+		if (mWidgets[i]->GetSelectable())
+		{
+			console::SetColor(FOREGROUND_BLUE);
+			if (i == mSelectableIndices[mFocus])
+			{
+				console::SetColor(BACKGROUND_GREEN);
+			}
+			
+			
+		}
+		cout << mWidgets[i]->GetString();
 	}
 
     cur.X = 0;
     cur.Y = mHeight-1;
-	console::SetColor(FOREGROUND_BLUE | BACKGROUND_RED);
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
+	console::SetColor(7);
     cout << setw(mWidth) << setfill('=') << "";
-    //cout << setfill(' ');
-
+	
 }
 
-void Screen::AddWidget(const Widget& obj) {
+void Screen::AddWidget(Widget* obj) {
 
 	mWidgets.push_back(obj);
+	SetSelectableIndices();
 
 }
 
-Widget::Widget(int x, int y)
-	: mX(x)
-	, mY(y)
-	, mSelectable(false)
-	, mStringData("")
-{
-}
-
-Widget::Widget(const string& s, int x, int y) : mX(x), mY(y), mSelectable(false), mStringData(s) {
-
-}
-
-Widget::~Widget()
-{
-}
-
-Button::Button(int x, int y)
-	:Widget(x, y)
-	, mNext(NULL)
-{
-}
-
-Button::Button(const string& s, int x, int y)
-	: Widget(s, x, y)
-	, mNext(NULL)
-{
-}
-
-Button::Button(Screen* next, const string& s, int x, int y)
-	: Widget(s, x, y)
-	, mNext(next)
-{
-}
-
-Button::~Button()
-{
-}
--- TypingTrainer/TypingTrainer.cpp
+++ TypingTrainer/TypingTrainer.cpp
@@ -4,8 +4,10 @@
 #include <conio.h>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
 #include "Screen.h"
 #include "Utill.h"
+
 
 using namespace std;
 // https://media.discordapp.net/attachments/293378188979994624/667216288782876702/unknown.png?width=481&height=670
@@ -21,48 +23,86 @@ using namespace std;
 int main() 
 {
 
-	FileManager manager;
+	//FileManager manager;
 
-	manager.RoadRanking();
-	manager.ShowRanking();
-	manager.Realignment('A', "UP");
-	manager.ShowRanking();
+	//manager.RoadRanking();
+	//manager.ShowRanking();
+	//manager.Realignment('A', "UP");
+	//manager.ShowRanking();
 	//manager.SaveRanking();
 
 	////FileManager manager;
- //    Screen sc(60,40);
- //    Widget t1("Typing Trainer", 20, 20);
- //   
- //    sc.AddWidget(t1);
- //    sc.Show();
- //    char a;
- //    cin >> a;
+     Screen sc(60,40);
+     Screen sc2(60, 40);
+     Screen sc3(60, 40);
+     Widget* t1 = new Widget("Typing Trainer", 20, 20);
+     Widget* t2 = new Button(&sc2, "screen2", 20, 30);
+     Widget* t3 = new Button(&sc3, "screen3", 20, 32);
+     sc.AddWidget(t1);
+     sc.AddWidget(t2);
+     sc.AddWidget(t3);
+     sc.SetFocus(1);
+     //sc.Show();
+     //char a;
+     //cin >> a;
    
-    //char c;
-    //vector<char> buf;
-    //while (true) 
-    //{ 
-    //    c = _getch();
-    //    if (c == 27) {
-    //        break;
-    //    }
+    char c;
+    vector<char> buf;
+    
+    Screen current;
+    current = sc;
+    const vector<int>* selectableIndices = current.GetSelectableIndices();
+    while (true) 
+    { 
+        current.Show();
+        c = _getch();
+        if (c == 27) {
+            break;
+        }
+        //if (c == '\b') {
+        //    if (buf.empty() == false) {
+        //        buf.pop_back();
+        //    }
+        //    //PrintBuf(buf);
+        //    cout << "\b \b";
+        //}
+        //else if (c == 13) {
+        //    cout << endl;
+        //}
+        //else {
+        //    buf.push_back(c);
+        //    cout << c;
+        //}
+        if (c == 72)
+        {
+            // upwards arrow
+            int size = selectableIndices->size();
+            int focus = current.GetFocus();
+            if (focus > 0)
+            {
+                current.SetFocus(focus - 1);
+            }
 
-    //    if (c == '\b') {
-    //        if (buf.empty() == false) {
-    //            buf.pop_back();
-    //        }
-    //        //PrintBuf(buf);
-    //        cout << "\b \b";
-    //    }
-    //    else if (c == 13) {
-    //        cout << endl;
-    //    }
-    //    else {
-    //        buf.push_back(c);
-    //        cout << c;
-    //    }
-    //
-    //}
+        } 
+        else if (c == 80)
+        {
+            // downwards arrow
+            int size = selectableIndices->size();
+            int focus = current.GetFocus();
+            if (focus < size - 1)
+            {
+                current.SetFocus(focus + 1);
+            }
+        }
+        else if (c == 32)
+        {
+            int focus = current.GetFocus();
+            current = *current.GetWidgets()[selectableIndices->at(focus)]->GetNext();
+            selectableIndices = current.GetSelectableIndices();
+        }
+        
+    
+    }
     return 0;
 }
 
